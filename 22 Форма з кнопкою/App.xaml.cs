feb4eb2905812ebfe33a8be63a66006ce274b58a
George <yu_tar@ukr.net>
2025-02-03 15:30:40 +0200

    Добавьте файлы проекта.

---
+++ 22 Форма з кнопкою/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _22_Форма_з_кнопкою
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
