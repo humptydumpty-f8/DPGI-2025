0e0a8881eb796c0a823679124448cfa94012f67c
George <yu_tar@ukr.net>
2025-03-24 15:15:38 +0200

    #7

---
+++ 6.15.Шаблонування списків/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _6._15.Шаблонування_списків
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 70 Створення RoutedCommand 1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _70_Створення_RoutedCommand_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //1-визначення команди
+        public static RoutedCommand CustomRoutedCommand = new RoutedCommand();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //2-створення обробників подій
+        private void ExecutedCustomCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Custom Command Executed");
+        }
+
+        private void CanExecuteCustomCommand(object sender, CanExecuteRoutedEventArgs e)
+        {
+            Control target = e.Source as Control;
+
+            if (target != null)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+    }
+}
