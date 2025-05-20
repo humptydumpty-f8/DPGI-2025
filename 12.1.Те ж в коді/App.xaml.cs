7663dfc225d205e113e229c428667e7a56d5acaa
George <yu_tar@ukr.net>
2025-05-20 13:19:02 +0300

    #12

---
+++ 12.1.Те ж в коді/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _12._1.Те_ж_в_коді
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
